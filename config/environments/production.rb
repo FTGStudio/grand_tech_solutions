133d70e84378cd49752189e5d7e9f1448ed85599
Michael Doran <doranm09@gmail.com>
2019-11-12 18:53:43 -0500

    adding host back in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'glacial-beyond-21612.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
